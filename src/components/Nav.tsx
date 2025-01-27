674878badd2fc92081a13191f7eea9ec5d7eb8ed
Elhadj Bah <[elhadjamadoubah@gmail.com]>
2025-01-26 21:07:58 -0700

    create .env file, add code to display navBar and Links between the pages. Also create candidate Interface

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,44 @@
+import { Link, useLocation } from 'react-router-dom';
+
 const Nav = () => {
+  const currentPage = useLocation().pathname;
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
+    <div>
+      <nav>
+       <ul>
+
+        <li>
+          <Link to="/" className={currentPage === '/' ? 'active' : ''}>
+            Home
+          </Link> 
+        </li>
+
+        <li>
+          <Link to="/ErrorPage" className={currentPage === '/ErrorPage' ? 'active' : ''}>
+            Error Page
+          </Link>
+        </li>
+
+        <li>
+          <Link to="/CandidateSearch" className={currentPage === '/CandidateSearch' ? 'active' : ''}>
+            Candidate Search
+          </Link>
+        </li>
+
+        <li>
+
+          <Link to="/SavedCandidates" className={currentPage === '/SavedCandidates' ? 'active' : ''}>
+            Saved Candidates
+          </Link>
+        </li>
+
+     
+        </ul>
+      </nav>
+      
+      
+      </div>
   )
 };
 
--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,13 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+
+export interface Candidate {
+  id: number;
+  firstName: string;
+  lastName: string;
+  userName: string;
+  location: string;
+  avatar: string;
+  email: string;
+  html_url: string;
+ company: string;
+}
